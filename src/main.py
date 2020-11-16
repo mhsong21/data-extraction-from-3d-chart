a7d077bb52d632f07c9eecad10546f2f817b2e67
mhsong <sniaw21@gmail.com>
2020-11-16 20:14:36 +0900

    Implement tick to value roughly

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
-import ocr_test from ocr_test
+import ocr
+
 
 def main():
-    ocr_test()
+    chart_path = './data/excel_chart_1.png'
+    box_path = './CRAFT-pytorch/result/res_excel_chart_1.txt'
+    axis_list = [
+        [52, 743, 52, 285],
+        [52, 743, 1252, 743]
+    ]
+    num_ticks = [11, 11]
+
+    result = ocr.tick_to_value(chart_path, box_path, axis_list, num_ticks)
+    print(result)
 
 
 if __name__ == "__main__":
