270f39db05ea5c9b9cef0616717a68dd79fddc09
elzino <elzinomaster@gmail.com>
2020-11-24 18:19:45 +0900

    changed evaluate

--- src/evaluate.py
+++ src/evaluate.py
@@ -15,11 +15,12 @@ def evaluate(predict_csv_path, ground_truth_csv_path):
     print("-------------------------------------\n")
 
     gt_npy = gt_df.to_numpy()
-    error = (gt_df - predict_df).to_numpy()
+    pred_npy = predict_df.to_numpy()
+    error = pred_npy - gt_npy
     mean_error_rate = np.nanmean(np.abs(error) / np.abs(gt_npy))
     print("mean_error_rate : ", format(mean_error_rate * 100, ".3f"), "%")
 
 
 
 if __name__ == "__main__":
-    evaluate("../predict/Matlab20.csv", "../data/ground_truth/Matlab20.csv")
+    evaluate("../result/pred_Matlab16.csv", "../data/ground_truth/Matlab16.csv")
