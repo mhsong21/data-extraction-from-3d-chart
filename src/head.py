cc8b6e899f8999db52fbdbd230ff4f2a099f5994
taxis <taxis853@gmail.com>
2020-12-02 01:03:58 +0900

    head.py final

--- src/head.py
+++ src/head.py
@@ -19,15 +19,19 @@ def template_matching(img, res, template, min_interval, vertex):
     src = res
     dst = img.copy()
     result = cv2.matchTemplate(src, template, cv2.TM_CCOEFF_NORMED)
-
+    print(min_interval)
     # using threshold
-    threshold = 0.7
+    threshold = 0.9
     loc = np.where(result >= threshold)
-
+    
+    while len(loc[0]) == 0:
+        threshold = threshold - 0.03
+        loc = np.where(result >= threshold)
+    
     # elements of coord[0] are x-coordinates, coord[1] are y-coordinates
     coord = np.zeros((2, len(loc[0])))
+    
     i = 0
-
     for pt in zip(*loc[::-1]):
         coord[0][i] = pt[0]
         coord[1][i] = pt[1]
@@ -38,56 +42,96 @@ def template_matching(img, res, template, min_interval, vertex):
     # get rid of nearby templates and save it on the coordinate
 
     coordinate = coord
+
     for i in range(coord.shape[1]):
         for j in range(coord.shape[1]):
             if coord[0][j] - 3 <= coord[0][i] <= coord[0][j] + 3:
                 if result[coord[1][j]][coord[0][j]] > result[coord[1][i]][coord[0][i]]:
                     coordinate[:, i] = coord[:, j]
     coordinate = np.unique(coordinate, axis=1)
-
-    if coordinate.shape[1] <= 1:
+    '''
+    if coordinate.shape[1] == 1:
         coordinate[:,0] += vertex
         return dst, coordinate, min_interval
+    '''
+    if coordinate.shape[1] == 0:
+        coordinate = np.zeros((0,0))
+        return dst, coordinate, min_interval
 
-
-    if coordinate.shape[0] > 1:
-        for i in range(coordinate.shape[0]-1):
+    if coordinate.shape[1] > 1:
+        for i in range(coordinate.shape[1]-1):
             if (coordinate[0][i+1] - coordinate[0][i]) < min_interval:
                 min_interval = coordinate[0][i+1] - coordinate[0][i]
+    original_min_interval = min_interval
+
+    # to avoid the situation if one of every two or three heads are detected. 
+    min_interval = min_interval // 6
+    
     if min_interval == 0:
-        min_interval = 1
+        min_interval = 5
     x_coord = coordinate[0][0]
+    '''
+    threshold = 0.25
     if min_interval < 5000:
         while x_coord < result.shape[1]:
-            if np.amax(result[:, x_coord]) > 0.1:
+            if np.amax(result[:, x_coord]) > threshold:
                 y_coord = np.argmax(result[:, x_coord])
                 coordinate = np.append(
                     coordinate, [[x_coord], [y_coord]], axis=1)
             x_coord += min_interval
+            
         x_coord = coordinate[0][0]
         while x_coord > 0:
-            if np.amax(result[:, x_coord]) > 0.1:
+            if np.amax(result[:, x_coord]) > threshold:
                 y_coord = np.argmax(result[:, x_coord])
                 coordinate = np.append(
                     coordinate, [[x_coord], [y_coord]], axis=1)
             x_coord -= min_interval
     coord = coordinate
+    '''
+
+    threshold = 0.25
+    if min_interval < 5000:
+        while x_coord+vertex[0] < result.shape[1]:
+            for i in range(x_coord-vertex[0]//2, x_coord+vertex[0]//2):
+                if np.amax(result[:, i]) > threshold:
+                    y_coord = np.argmax(result[:, i])
+                    coordinate = np.append(
+                        coordinate, [[i], [y_coord]], axis=1)
+            x_coord += min_interval    
+            
+        x_coord = coordinate[0][0]
+        while x_coord-vertex[0] > 0:
+            for i in range(x_coord-vertex[0]//2, x_coord+vertex[0]//2):
+                if np.amax(result[:, i]) > threshold:
+                    y_coord = np.argmax(result[:, i])
+                    coordinate = np.append(
+                        coordinate, [[i], [y_coord]], axis=1)
+            x_coord -= min_interval
+    coord = coordinate
+
     for i in range(coord.shape[1]):
         for j in range(coord.shape[1]):
-            if coord[0][j] - template.shape[1] - 5 <= coord[0][i] <= coord[0][j] + template.shape[1] + 5:
+            if coord[0][j] - min(template.shape[1], min_interval*5) <= coord[0][i] <= coord[0][j] + min(template.shape[1], min_interval*5):
                 if result[coord[1][j]][coord[0][j]] > result[coord[1][i]][coord[0][i]]:
                     coordinate[:, i] = coord[:, j]
     coordinate = np.unique(coordinate, axis=1)
 
+    if coordinate.shape[1] > 1:
+        for i in range(coordinate.shape[1]-1):
+            if (coordinate[0][i+1] - coordinate[0][i]) < original_min_interval and (coordinate[0][i+1] - coordinate[0][i]) > min(template.shape[1], min_interval):
+                original_min_interval = coordinate[0][i+1] - coordinate[0][i]
+    min_interval = original_min_interval
+    
     # dst is the image which is marked where the template is matched
     h, w, _ = template.shape
     for i in range(coordinate.shape[1]):
         cv2.rectangle(dst, (coordinate[0][i], coordinate[1][i]),
-                      (coordinate[0][i] + w, coordinate[1][i] + h), (0, 0, 255), 1)
+                      (coordinate[0][i] + w, coordinate[1][i] + h), (0, 0, 0), 2)
     
     for i in range(coordinate.shape[1]):
         coordinate[:,i] += vertex
-    
+
     cv2.imshow("dst", dst)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -95,7 +139,7 @@ def template_matching(img, res, template, min_interval, vertex):
     return dst, coordinate, min_interval
 
 
-def template_finding(img, res, bar_colors):
+def template_finding(img, res, bar_colors, maxsize_template):
     ####
     # codes for finding a template (head)
     ####
@@ -129,7 +173,7 @@ def template_finding(img, res, bar_colors):
             x = x[0][0]
             y = j
             break
-    print(x,y)
+    #print(x,y)
     # the coordinate of head top point is saved in x, y
     # x -= 1
     # y -= 1
@@ -178,9 +222,12 @@ def template_finding(img, res, bar_colors):
                               x_alongx + x_alongy - x, y_alongx + y_alongy - y]])
     template = res[np.amin(template_coord[:, 1]): np.amax(
         template_coord[:, 1]) + 1, np.amin(template_coord[:, 0]): np.amax(template_coord[:, 0]) + 1, :]
-    
+    if template.shape[1] < maxsize_template.shape[1] or template.shape[0] > res.shape[0] * 0.2:
+        template = np.where(maxsize_template[:,:] == [0,0,0], [0,0,0], res[y][x])
+    template = template.astype(np.uint8)
+    #np.all([maxsize_template[:,:,0] == 0,maxsize_template[:,:,1] == 0,maxsize_template[:,:,2] == 0]) 
     vertex = np.array([x_alongy - x, template.shape[0]])
-    
+    #print(template.shape)
     cv2.imshow("template", template)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -215,22 +262,26 @@ def run(filename):
 
     for i, igs in enumerate(result):
         result[i] = edge_enhance(igs)
-
+    template = np.zeros((0,0,3))
     for i in range(number_colors):
         res = Image.fromarray(result[i])
         image_name = "color_divided/"+filename+"color_%i.png" % i
         res.save(image_name)
-
+        
         template, vertex = template_finding(
-            img, result[i], bar_colors[i])
-        image_name = folder+"/template_%i.png" % i
-        cv2.imwrite(image_name, template)
+            img, result[i], bar_colors[i], template)
+        #image_name = folder+"/template_%i.png" % i
+        #cv2.imwrite(image_name, template)
 
         _, _, min_interval = template_matching(
             img, result[i], template, min_interval, vertex)
 
     for i in range(number_colors):
-        template = cv2.imread(folder+"/template_%i.png" % i)
+        template, vertex = template_finding(
+            img, result[i], bar_colors[i], template)
+        image_name = folder+"/template_%i.png" % i
+        cv2.imwrite(image_name, template)
+        #template = cv2.imread(folder+"/template_%i.png" % i)
         matched_image, coord, min_interval = template_matching(
             img, result[i], template, min_interval, vertex)
         template_coordinate.append(coord)
